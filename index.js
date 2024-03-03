59c0ceffe93054c8a47f15f90d7f48dc83616d86
Vincent <vincentrenaud36@gmail.com>
2024-03-03 16:42:19 +0100

    Fourth commit

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const sequelize = require('./sequelize');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 
 // Import Routes
 const helloRoute = require('./src/routes/routes');
 const registerRoute = require('./src/routes/routes');
-
-
-app.use(express.json());
+const loginRoute = require('./src/routes/routes');
+const profilRoute = require('./src/routes/routes');
+const fileRoute = require('./src/routes/routes');
+const userRoute = require('./src/routes/routes');
 
 app.use('/', helloRoute);
 app.use('/', registerRoute);
+app.use('/', loginRoute);
+app.use('/', profilRoute);
+app.use('/', fileRoute);
+app.use('/users', userRoute);
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+
+require('./passport');
 
 (async () => {
   try {
@@ -26,5 +41,3 @@ app.use('/', registerRoute);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
-
-  
